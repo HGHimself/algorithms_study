17ee7ddb5a2058b8cf1cb478fcff4409985f4a72
MosesHimself <hmkwp5@mail.missouri.edu>
2018-09-18 10:44:36 -0500

    gcd works for now

--- gcd.c
+++ gcd.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <float.h>
 
 double gcd(double a, double b);
-double mod(double a, double b);
+double mod(double x, double y);
 
 int main(void)  {
   printf("\n\nHello World!!\n\nWe are studying gcd here.\n\n");
@@ -15,27 +16,54 @@ int main(void)  {
 
   double a = 1764218677667;
   double b = 208095570762977;
-
+  //double a = 691;
+  //double b = 10;
+  //printf("\n%f - %f = %f\n", b, a, b - a);
   //double a = 691;
   //double b = 10;
 
-  printf("%f mod %f = %f", a, b, mod(a, b));
-  /*
+  //printf("%f mod %f = %f", a, b, gcd(a, b));
+
   start = clock();
   res = gcd(a, b);
   end = clock();
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
   printf("gcd(%f, %f) = %f, %f seconds\n\n", a, b, res, cpu_time_used);
-  */
+
 
 }
 
 double gcd(double a, double b) {
-    if (0 == a) return b;
-    return gcd(b / a, a);
+  if (0 == a || a == b) return b;
+  double q, r;
+  if(a > b)  {
+    r = mod(a, b);
+    q = (a - r) / b;
+  }
+  else  {
+    r = mod(b, a);
+    q = (b - r) / a;
+  }
+  printf("%f = (%f)(%f) + %f\n", a, b, q, r);
+
+
+  return gcd(r, a);
 }
 
-double mod(double a, double b)  {
-  //a / b = q/b + r/b
-  return (a / b);
+double mod(double x, double y) {
+  int64_t modulus = x, divisor = y;
+
+  while (divisor <= modulus && divisor <= DBL_MAX/2)  {
+    divisor <<= 1;
+  }
+
+
+  while (modulus >= y) {
+    while (divisor > modulus)  {
+      divisor >>= 1;
+    }
+    modulus -= divisor;
+  }
+
+  return modulus;
 }
--- mod.c
+++ mod.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <float.h>
 
 double mod_basic(double a, double b);
+double mod_bld(double x, double y);
+double divide(double x, double y);
 
 int main(void)  {
   printf("\n\nHello World!!\n\nWe are studying mod here.\n\n");
@@ -12,29 +15,156 @@ int main(void)  {
   double cpu_time_used;
   double res;
 
-  double a = 1764218677667;
-  double b = 208095570762977;
+  double a = 208095570762977;
+  double b = 1764218677667;
 
   //double a = 691;
   //double b = 10;
-
-  printf("\n%f mod %f = %f\n", a, b, mod_basic(b, a));
   /*
   start = clock();
-  res = gcd(a, b);
+  res = mod_basic(a, b);
   end = clock();
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-  printf("gcd(%f, %f) = %f, %f seconds\n\n", a, b, res, cpu_time_used);
+  printf("mod_basic(%f, %f) = %f, %f seconds\n\n", a, b, res, cpu_time_used);
+
+  start = clock();
+  res = mod_bld(a, b);
+  end = clock();
+  cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+  printf("mod_bld(%f, %f) = %f, %f seconds\n\n", a, b, res, cpu_time_used);
   */
+  float result = mod_bld(27, 5);
+
 
 }
 
-/* Compute x mod y naively. */
+/*
+Compute x mod y naively.
+This is bad, runs in O(x/y) or O(n)
+*/
 double mod_basic(double x, double y) {
-    double result = x;
-    while (result >= y)  {
-      printf("result is %f, y is %f", result, y);
-      result -= y;
+  double result = x;
+  while (result >= y)  {
+    //printf("result is %f, y is %f\n", result, y);
+    result -= y;
+  }
+  return result;
+}
+
+/*
+Compute x mod y using binary long division.
+Stupid doubles dont allow for binary operations
+*/
+double mod_bld(double x, double y)
+{
+  int64_t modulus = x, divisor = y;
+
+  while (divisor <= modulus && divisor <= 2147483647/2)  {
+    printf("divisor is %lld\n", divisor);
+    divisor <<= 1;
+  }
+
+
+  while (modulus >= y) {
+    printf("modulus is %lld\n", modulus);
+    while (divisor > modulus)  {
+      printf("divisor is %lld\n", divisor);
+      divisor >>= 1;
     }
-    return result;
+    modulus -= divisor;
+  }
+
+  return modulus;
+}
+
+void unsigned_divide(unsigned int dividend,
+		     unsigned int divisor,
+		     unsigned int &quotient,
+		     unsigned int &remainder )
+{
+  unsigned int t, num_bits;
+  unsigned int q, bit, d;
+  int i;
+
+  remainder = 0;
+  quotient = 0;
+
+  if (divisor == 0)
+    return;
+
+  if (divisor > dividend) {
+    remainder = dividend;
+    return;
+  }
+
+  if (divisor == dividend) {
+    quotient = 1;
+    return;
+  }
+
+  num_bits = 32;
+
+  while (remainder < divisor) {
+    bit = (dividend & 0x80000000) >> 31;
+    remainder = (remainder << 1) | bit;
+    d = dividend;
+    dividend = dividend << 1;
+    num_bits--;
+  }
+
+
+  /* The loop, above, always goes one iteration too far.
+     To avoid inserting an "if" statement inside the loop
+     the last iteration is simply reversed. */
+
+  dividend = d;
+  remainder = remainder >> 1;
+  num_bits++;
+
+  for (i = 0; i < num_bits; i++) {
+    bit = (dividend & 0x80000000) >> 31;
+    remainder = (remainder << 1) | bit;
+    t = remainder - divisor;
+    q = !((t & 0x80000000) >> 31);
+    dividend = dividend << 1;
+    quotient = (quotient << 1) | q;
+    if (q) {
+       remainder = t;
+     }
+  }
+}  /* unsigned_divide */
+
+
+void signed_divide(int dividend,
+		   int divisor,
+		   int &quotient,
+		   int &remainder )
+{
+  unsigned int dend, dor;
+  unsigned int q, r;
+
+  dend = absolute_value(dividend);
+  dor  = absolute_value(divisor);
+  unsigned_divide( dend, dor, q, r );
+
+  /* the sign of the remainder is the same as the sign of the dividend
+     and the quotient is negated if the signs of the operands are
+     opposite */
+  quotient = q;
+  if (dividend < 0) {
+    remainder = -r;
+    if (divisor > 0)
+      quotient = -q;
+  }
+  else { /* positive dividend */
+    remainder = r;
+    if (divisor < 0)
+      quotient = -q;
+  }
+
+} /* signed_divide */
+
+double absolute_value(double x)  {
+  if(x >= 0) return x;
+  else return -x;
 }
